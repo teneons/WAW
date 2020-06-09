aab714317e685c66842b2415daf13272c12a085f
teneons <teneons@gmail.com>
2020-06-09 23:26:59 +0300

    StartInitSetup prj

---
+++ script.js
@@ -0,0 +1,2 @@
+// fetch('http://api.openweathermap.org/data/2.5/weather?q=Lviv&lang=ua&units=metric&appid=2a4a207e504ae6c48ab505a543702a9d')
+//     .then(data => data.json()).then(data => console.log(data))

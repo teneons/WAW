6a75c290b4ec91499df5a6aa09035989a8fef885
teneons <teneons@gmail.com>
2020-06-10 18:54:02 +0300

    maked 1st part - weather

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
+document.querySelector('.InputMessageBox').style.visibility = 'hidden';
+
 // fetch('http://api.openweathermap.org/data/2.5/weather?q=Lviv&lang=ua&units=metric&appid=2a4a207e504ae6c48ab505a543702a9d')
 //     .then(data => data.json()).then(data => console.log(data))

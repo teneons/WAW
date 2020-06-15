7243e4a0580a07afbe2d8fba301c99cdc1e66a62
teneons <teneons@gmail.com>
2020-06-15 20:43:24 +0300

    clear/test some in code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.querySelector('.InputMessageBox').style.visibility = 'hidden';
 
+document.querySelector('.Temperature').innerHTML =  + '<sup class="TemprtrSup">&degC</sup>';
+
 // fetch('http://api.openweathermap.org/data/2.5/weather?q=Lviv&lang=ua&units=metric&appid=2a4a207e504ae6c48ab505a543702a9d')
 //     .then(data => data.json()).then(data => console.log(data))

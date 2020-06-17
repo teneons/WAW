8fb397fbc80e5777e71bb03f18b688a259e72a7f
teneons <teneons@gmail.com>
2020-06-18 00:42:35 +0300

    optimized wthr. icons,bettered few code

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ async function queryWeather() {
     }
 }
 
+//checking field at - empty
 function auditField () {
     if(InputCity.value == '') {
         InputMessageBox.style.visibility = 'visible';
@@ -43,12 +44,13 @@ function auditField () {
     }
 }
 
+//btn actions
 SearchBtn.addEventListener('click', auditField)
 InputCity.addEventListener('keydown', (event) => {
     if(event.key == 'Enter') auditField();
 })
 
-
+//show data on card
 function setWeatherData (dataWeather) {
                                                                                                     console.log(dataWeather)
     NameCity.innerHTML = `${dataWeather.name}, ${dataWeather.sys.country}`;
@@ -57,46 +59,23 @@ function setWeatherData (dataWeather) {
         StateWeather.innerHTML = i.description;
 
         if(i.icon == '01d') WeatherIcon.style.backgroundImage = 'url(style/01d.png)';
-        if(i.icon == '01n') WeatherIcon.style.backgroundImage = 'url(style/02d.png)';
+        if(i.icon == '01n') WeatherIcon.style.backgroundImage = 'url(style/01n.png)';
         if(i.icon == '02d') WeatherIcon.style.backgroundImage = 'url(style/02d.png)';
-        if(i.icon == '02n') WeatherIcon.style.backgroundImage = 'url(style/01d.png)';
-        if(i.icon == '03d') WeatherIcon.style.backgroundImage = 'url(style/03d.png)';
-        if(i.icon == '03n') WeatherIcon.style.backgroundImage = 'url(style/03d.png)';
-        if(i.icon == '04d') WeatherIcon.style.backgroundImage = 'url(style/04d.png)';
-        if(i.icon == '04n') WeatherIcon.style.backgroundImage = 'url(style/04d.png)';
-        if(i.icon == '09d') WeatherIcon.style.backgroundImage = 'url(style/09d.png)';
-        if(i.icon == '09n') WeatherIcon.style.backgroundImage = 'url(style/09d.png)';
+        if(i.icon == '02n') WeatherIcon.style.backgroundImage = 'url(style/02n.png)';
+        if(i.icon == '03d') WeatherIcon.style.backgroundImage = 'url(style/03n.png)';
+        if(i.icon == '03n') WeatherIcon.style.backgroundImage = 'url(style/03n.png)';
+        if(i.icon == '04d') WeatherIcon.style.backgroundImage = 'url(style/04n.png)';
+        if(i.icon == '04n') WeatherIcon.style.backgroundImage = 'url(style/04n.png)';
+        if(i.icon == '09d') WeatherIcon.style.backgroundImage = 'url(style/09n.png)';
+        if(i.icon == '09n') WeatherIcon.style.backgroundImage = 'url(style/09n.png)';
         if(i.icon == '10d') WeatherIcon.style.backgroundImage = 'url(style/10d.png)';
         if(i.icon == '10n') WeatherIcon.style.backgroundImage = 'url(style/10d.png)';
-        if(i.icon == '11d') WeatherIcon.style.backgroundImage = 'url(style/11d.png)';
-        if(i.icon == '11n') WeatherIcon.style.backgroundImage = 'url(style/11d.png)';
+        if(i.icon == '11d') WeatherIcon.style.backgroundImage = 'url(style/11n.png)';
+        if(i.icon == '11n') WeatherIcon.style.backgroundImage = 'url(style/11n.png)';
         if(i.icon == '13d') WeatherIcon.style.backgroundImage = 'url(style/13d.png)';
         if(i.icon == '13n') WeatherIcon.style.backgroundImage = 'url(style/13d.png)';
-        if(i.icon == '50d') WeatherIcon.style.backgroundImage = 'url(style/50d.png)';
-        if(i.icon == '50n') WeatherIcon.style.backgroundImage = 'url(style/50d.png)';
+        if(i.icon == '50d') WeatherIcon.style.backgroundImage = 'url(style/50n.png)';
+        if(i.icon == '50n') WeatherIcon.style.backgroundImage = 'url(style/50n.png)';
     }
     Temperature.innerHTML = dataWeather.main.temp + '<sup class="TemprtrSup">&degC</sup>';
 }
-
-
-
-        // switch(i.icon) {
-        //     case '01d': WeatherIcon.style.backgroundImage = 'url(style/01d.png)';
-        //     case '01n': WeatherIcon.style.backgroundImage = 'url(style/01n.png)';
-        //     case '02d': WeatherIcon.style.backgroundImage = 'url(style/02d.png)';
-        //     case '02n': WeatherIcon.style.backgroundImage = 'url(style/02n.png)';
-        //     case '03d': WeatherIcon.style.backgroundImage = 'url(style/03d.png)';
-        //     case '03n': WeatherIcon.style.backgroundImage = 'url(style/03n.png)';
-        //     case '04d': WeatherIcon.style.backgroundImage = 'url(style/04d.png)';
-        //     case '04n': WeatherIcon.style.backgroundImage = 'url(style/04n.png)';
-        //     case '09d': WeatherIcon.style.backgroundImage = 'url(style/09d.png)';
-        //     case '09n': WeatherIcon.style.backgroundImage = 'url(style/09n.png)';
-        //     case '10d': WeatherIcon.style.backgroundImage = 'url(style/10d.png)';
-        //     case '10n': WeatherIcon.style.backgroundImage = 'url(style/10n.png)';
-        //     case '11d': WeatherIcon.style.backgroundImage = 'url(style/11d.png)';
-        //     case '11n': WeatherIcon.style.backgroundImage = 'url(style/11n.png)';
-        //     case '13d': WeatherIcon.style.backgroundImage = 'url(style/13d.png)';
-        //     case '13n': WeatherIcon.style.backgroundImage = 'url(style/13n.png)';
-        //     case '50d': WeatherIcon.style.backgroundImage = 'url(style/50d.png)';
-        //     case '50n': WeatherIcon.style.backgroundImage = 'url(style/50n.png)';
-        // }

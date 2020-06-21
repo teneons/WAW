88662f0c571fa9451779b11cc4d2435263acc40c
teneons <teneons@gmail.com>
2020-06-21 22:24:53 +0300

    optimized & bettered work card, weather block

--- script.js
+++ script.js
@@ -18,15 +18,17 @@ let InputMessageBox = document.querySelector('.InputMessageBox');
 let WeatherCard = document.querySelector('.WeatherCard');
 let NameCity = document.querySelector('.NameCity');
 let WeatherIcon = document.querySelector('.WeatherIcon');
+let JustIcon = document.querySelector('.JustIcon');
 let StateWeather = document.querySelector('.StateWeather');
 let Temperature = document.querySelector('.Temperature');
+let TemprtrSup = document.querySelector('.TemprtrSup');
 
 
 //query
 async function queryWeather() {
     try{
         InputMessageBox.style.visibility = 'hidden';
-        const getJsonWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${InputCity.value}&lang=${SiteLang}&units=${UnitsImperial}&appid=${apiId}`);
+        const getJsonWeather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${InputCity.value}&lang=${SettingsSiteLang}&units=${SettingsUnitsMetric}&appid=${apiId}`);
         const data = await getJsonWeather.json();
         return await setWeatherData(data);
     } catch (e) {
@@ -56,30 +58,46 @@ InputCity.addEventListener('keydown', (event) => {
 
 //show data on card
 function setWeatherData (dataWeather) {
-                                                                                                    console.log(dataWeather)
+                                        console.log(dataWeather)
+    //out city name
     NameCity.innerHTML = `${dataWeather.name}, ${dataWeather.sys.country}`;
 
-    for(let i of dataWeather.weather){
+    //out icon and description (because they are into one array)
+    for(let i of dataWeather.weather) {
         StateWeather.innerHTML = i.description;
 
-        if(i.icon == '01d') WeatherIcon.style.backgroundImage = 'url(media/01d.png)';
-        if(i.icon == '01n') WeatherIcon.style.backgroundImage = 'url(media/01n.png)';
-        if(i.icon == '02d') WeatherIcon.style.backgroundImage = 'url(media/02d.png)';
-        if(i.icon == '02n') WeatherIcon.style.backgroundImage = 'url(media/02n.png)';
-        if(i.icon == '03d') WeatherIcon.style.backgroundImage = 'url(media/03n.png)';
-        if(i.icon == '03n') WeatherIcon.style.backgroundImage = 'url(media/03n.png)';
-        if(i.icon == '04d') WeatherIcon.style.backgroundImage = 'url(media/04n.png)';
-        if(i.icon == '04n') WeatherIcon.style.backgroundImage = 'url(media/04n.png)';
-        if(i.icon == '09d') WeatherIcon.style.backgroundImage = 'url(media/09n.png)';
-        if(i.icon == '09n') WeatherIcon.style.backgroundImage = 'url(media/09n.png)';
-        if(i.icon == '10d') WeatherIcon.style.backgroundImage = 'url(media/10d.png)';
-        if(i.icon == '10n') WeatherIcon.style.backgroundImage = 'url(media/10d.png)';
-        if(i.icon == '11d') WeatherIcon.style.backgroundImage = 'url(media/11n.png)';
-        if(i.icon == '11n') WeatherIcon.style.backgroundImage = 'url(media/11n.png)';
-        if(i.icon == '13d') WeatherIcon.style.backgroundImage = 'url(media/13d.png)';
-        if(i.icon == '13n') WeatherIcon.style.backgroundImage = 'url(media/13d.png)';
-        if(i.icon == '50d') WeatherIcon.style.backgroundImage = 'url(media/50n.png)';
-        if(i.icon == '50n') WeatherIcon.style.backgroundImage = 'url(media/50n.png)';
+        if(i.icon == '01d') JustIcon.style.backgroundImage = 'url(media/01d.png)';
+        if(i.icon == '01n') JustIcon.style.backgroundImage = 'url(media/01n.png)';
+        if(i.icon == '02d') JustIcon.style.backgroundImage = 'url(media/02d.png)';
+        if(i.icon == '02n') JustIcon.style.backgroundImage = 'url(media/02n.png)';
+        if(i.icon == '03d') JustIcon.style.backgroundImage = 'url(media/03n.png)';
+        if(i.icon == '03n') JustIcon.style.backgroundImage = 'url(media/03n.png)';
+        if(i.icon == '04d') JustIcon.style.backgroundImage = 'url(media/04n.png)';
+        if(i.icon == '04n') JustIcon.style.backgroundImage = 'url(media/04n.png)';
+        if(i.icon == '09d') JustIcon.style.backgroundImage = 'url(media/09n.png)';
+        if(i.icon == '09n') JustIcon.style.backgroundImage = 'url(media/09n.png)';
+        if(i.icon == '10d') JustIcon.style.backgroundImage = 'url(media/10d.png)';
+        if(i.icon == '10n') JustIcon.style.backgroundImage = 'url(media/10d.png)';
+        if(i.icon == '11d') JustIcon.style.backgroundImage = 'url(media/11n.png)';
+        if(i.icon == '11n') JustIcon.style.backgroundImage = 'url(media/11n.png)';
+        if(i.icon == '13d') JustIcon.style.backgroundImage = 'url(media/13d.png)';
+        if(i.icon == '13n') JustIcon.style.backgroundImage = 'url(media/13d.png)';
+        if(i.icon == '50d') JustIcon.style.backgroundImage = 'url(media/50n.png)';
+        if(i.icon == '50n') JustIcon.style.backgroundImage = 'url(media/50n.png)';
     }
+
+    //out temperature
+    //#FFA724       #999999
+    if(dataWeather.main.temp < 0) {
+        //Temperature.style.color = '#999999';
+        console.log('cold')
+        if(dataWeather.main.temp >= 0) {
+            //Temperature.style.color = '#FFA724';
+            console.log('hot')
+        }
+    }
+
     Temperature.innerHTML = dataWeather.main.temp + '<sup class="TemprtrSup">&degC</sup>';
+
+    //
 }
